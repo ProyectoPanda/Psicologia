0567ab1a03a0626761fcad9e9f34ab6161835647
rober <proyecto.panda@hotmail.com>
2020-03-11 23:24:24 -0500

    2nd

---
+++ src/main/java/com/web/TestPsicologia/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.web.TestPsicologia;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestPsicologiaApplication.class);
+	}
+}
