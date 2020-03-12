29aced54fccb14b97a738bcef1fd60fdd7072591
rober <proyecto.panda@hotmail.com>
2020-03-11 23:41:05 -0500

    6to

--- src/main/java/com/web/TestPsicologia/TestPsicologiaApplication.java
+++ src/main/java/com/web/TestPsicologia/TestPsicologiaApplication.java
@@ -2,12 +2,24 @@ package com.web.TestPsicologia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class TestPsicologiaApplication {
 
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestPsicologiaApplication.class, args);
 	}
+	
+	
 
 }
